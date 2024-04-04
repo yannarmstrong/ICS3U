f30c64c9e35dd0d585d090fe3036543180e72ae1
yannarmstrong <yann.armstrong@stu.ocsb.ca>
2024-04-04 14:27:25 -0400

    more done

--- UNIT01/1.4-User_Input.js
+++ UNIT01/1.4-User_Input.js
@@ -2,7 +2,13 @@ const prompt = require('prompt-sync')();
 
 let birthyear = prompt("What year were you born? ");
 console.log("You entered " + birthyear);
-console.log("You are likely " + 2024-birthyear + " years old.");
+console.log("You are likely " + (2024-birthyear) + " years old.");
 
 let name = prompt("What is your name? ");
-console.log("Hi, " + name + ". You were born in " + birthyear + " and you are likely " + 2024-birthyear +" years old.");
+console.log("Hi, " + name + ". You were born in " + birthyear + " and you are likely " + (2024-birthyear) +" years old.");
+
+let x = name.length;
+console.log("Your name is " + x + " letters long.");
+
+let fahrenheit = prompt("Enter a temperature in degrees fahrenheit. ");
+console.log(fahrenheit + " degrees fahrenheit is " + ((fahrenheit-32)*5/9) + " degrees celcius.");
---
+++ UNIT01/1.5-Conditionals_1.js
@@ -0,0 +1,4 @@
+const prompt = require('prompt-sync')();
+
+let age = prompt("How many years old are you? ");
+age = number;
