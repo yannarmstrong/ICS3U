f38f335f25432db8ed16faeb90d03b4e0338a97b
Yann Armstrong <yann.armstrong@stu.ocsb.ca>
2024-04-15 14:08:12 -0400

    a

--- UNIT01/1.0-helloworld.js
+++ UNIT01/1.0-helloworld.js
@@ -1,2 +1,1 @@
 console.log("Hello Mr J's World");
-
--- UNIT01/1.5-Conditionals_2.js
+++ UNIT01/1.5-Conditionals_2.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 
 let user_name = prompt("Welcome, what is your name? ");
-let age = Number(prompt("Hello " + user_name + " how old are you? "));
+let age = parseInt(prompt("Hello " + user_name + " how old are you? "));
 
 if (age>=16){
     console.log("You are old enough to drive!");
@@ -15,3 +15,22 @@ console.log("Pick an option:\n1 - Play\n2 - Options\n3 - DLC\n4 - Check for Upda
 
 // Ask for selection
 let selection = parseInt(prompt("Hi " + user_name + ". Please make a selection "));
+
+if (selection==1){
+    console.log("Let's play!");
+} 
+else if (selection==2){
+    console.log("You selected Options.");
+}
+else if (selection==3){
+    console.log("No new DLC at this time.");
+}
+else if (selection==4){
+    console.log("Everything is up to date.");
+}
+else if (selection==5){
+    console.log("Bye!");
+}
+else{
+    console.log("Invalid selection.");
+}
