27e647ab6368ab23c5d645836725b0e95d7a794c
Yann Armstrong <yann.armstrong@stu.ocsb.ca>
2024-04-19 13:44:16 -0400

    1.6

--- UNIT01/1.6-Logical_Operators.js
+++ UNIT01/1.6-Logical_Operators.js
@@ -1,33 +1,29 @@
 const prompt = require('prompt-sync')();
 
-let answer = prompt("Do you want to play a game? (y/n): ");
+let answer = prompt("Do you want to play a game? y/n: ");
 
-if (answer == y){
+if (answer == "" || answer == "y" || answer == "Y") {
     let number = parseInt(prompt("Enter a number: "));
-    if (number>10 && number<50){
+    if (number > 10 && number < 50) {
         console.log("Your next number should be greater than 50. ");
     }
-    let number_two = partseInt(prompt("Enter a second number: "));
-    if (number_two % 2 == 0){
+    let number_two = parseInt(prompt("Enter a second number: "));
+    if (number_two % 2 == 0) {
         console.log("Your next number should be odd. ");
-    }
-    else if (number_two % 2 != 0){
+    } else {
         console.log("Your next number should be even. ");
     }
     let number_three = parseInt(prompt("Enter a third number: "));
-    if ((number+number_two+number_three+1)%10==0){
+    if ((number + number_two + number_three + 1) % 10 == 0) {
         console.log("Your numbers are Squirrelly. ");
-    }
-    else{
+    } else {
         console.log("Your numbers are not Squirrelly. ");
     }
-    if (number%3==0 || number_two%3==0 || number_three%3==0){
+    if ((number % 3 == 0 && number_two % 3 == 0) || (number % 3 == 0 && number_three % 3 == 0) || (number_two % 3 == 0 && number_three % 3 == 0) || (number % 3 == 0 && number_two % 3 == 0 && number_three % 3 == 0)) {
         console.log("Your numbers are Jazzy. ");
-    }
-    else{
+    } else {
         console.log("Your numbers are not Jazzy. ");
     }
-}
-else{
-    console.log("Sorry to hear that. ");
+} else {
+    console.log("Sorry to hear that, have a good day! ");
 }
