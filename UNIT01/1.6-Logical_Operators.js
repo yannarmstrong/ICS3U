7a7aa96216fda9665f6289513a0f977bc0141b0b
Yann Armstrong <yann.armstrong@stu.ocsb.ca>
2024-04-19 13:21:17 -0400

    k

---
+++ UNIT01/1.6-Logical_Operators.js
@@ -0,0 +1,33 @@
+const prompt = require('prompt-sync')();
+
+let answer = prompt("Do you want to play a game? (y/n): ");
+
+if (answer == y){
+    let number = parseInt(prompt("Enter a number: "));
+    if (number>10 && number<50){
+        console.log("Your next number should be greater than 50. ");
+    }
+    let number_two = partseInt(prompt("Enter a second number: "));
+    if (number_two % 2 == 0){
+        console.log("Your next number should be odd. ");
+    }
+    else if (number_two % 2 != 0){
+        console.log("Your next number should be even. ");
+    }
+    let number_three = parseInt(prompt("Enter a third number: "));
+    if ((number+number_two+number_three+1)%10==0){
+        console.log("Your numbers are Squirrelly. ");
+    }
+    else{
+        console.log("Your numbers are not Squirrelly. ");
+    }
+    if (number%3==0 || number_two%3==0 || number_three%3==0){
+        console.log("Your numbers are Jazzy. ");
+    }
+    else{
+        console.log("Your numbers are not Jazzy. ");
+    }
+}
+else{
+    console.log("Sorry to hear that. ");
+}
--- UNIT01/tempCodeRunnerFile.js
+++ UNIT01/tempCodeRunnerFile.js
@@ -1,9 +1,10 @@
-let random_number = Math.random() * 10;     // Get a single-digit value (with decimals)
-random_number = Math.floor(random_number);  // Chop off the decimals
-if (random_number==0 || random_number%2 == 0 || (random_number%2 == 0 && random_number%3 == 0) || (random_number==1 || random_number==2 || random_number==3 || random_number==5 || random_number==7)){
-    console.log("So.");
+const prompt = require('prompt-sync')();
+
+let answer = prompt("Do you want to play a game? (y/n): ");
+
+if (answer==$y){
+    let number = parseInt(prompt("Enter a number: "));
+    if (number>10 && number<50){
+        console.log("Your next number should be greater than 50. ");
+    }
 }
-else {
-    console.log("Notso.");
-}
-console.log(random_number);
