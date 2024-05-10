f526f290ad6235e1c212154f33661d140f66a2e1
Yann <Yann.armstrong@stu.ocsb.ca>
2024-05-10 13:55:06 -0400

    3.5 start

---
+++ UNIT03/3.5-Nested_Loops.js
@@ -0,0 +1,10 @@
+/**
+* 3.5 - Nested Loops
+* Author: YANN ARMSTRONG
+*
+* Lesson link:  https://gist.github.com/mrjamieson29/532d390d9ae11dde5f419cf8b661d7c3#file-3-5-nested-loops-md
+**/
+
+const prompt = require('prompt-sync')();
+
+function ()
